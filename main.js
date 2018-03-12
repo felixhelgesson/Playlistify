e6dff0b595ab9027f6d113f758a5a7a6b8aa06ef
LuddeW <ludwig.wiktorsson@gmail.com>
2018-03-12 13:40:06 +0100

    Adding site to webshare to make magic

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ $(document).ready(function () {
     console.log("ready");
 })
 
+
+
 var Playlistify = {
     oauth: function(){
-
+        var oauthURI = "https://accounts.spotify.com/authorize?client_id=7f034be8c85340a9a3179b195bfa343f&redirect_uri=http://WebShare.mah.se/af5392&response_type=token";
+        
     }
 }
 
