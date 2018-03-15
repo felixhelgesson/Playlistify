c5a6f3e42230104c94a6373be94b5350fe7485f3
Felix Helgesson <felix.helgesson@gmail.com>
2018-03-15 13:44:49 +0100

    Bootstrap

---
+++ tips.js
@@ -0,0 +1,19 @@
+// Randomize 3 kinds of playlists, Chill, Workout and Party.
+
+
+var app = {};
+app.uriUrl = 'https://api.spotify.com/v1/';
+
+
+
+app.events = function(){
+    $('form')
+}
+
+app.init = function(){
+
+};
+
+$(app.init);
+
+
