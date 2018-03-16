c14bfe5170cc5ecaabcd56f73514cb694e38b6cf
felixhelgesson <felix.helgesson@gmail.com>
2018-03-16 12:08:57 +0100

    Checkboxarray

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     Playlistify.init();
     console.log("ready");
-    
+
     //console.log(document.referrer);
     //console.log(Playlistify.getUrlVars());
 })
@@ -11,40 +11,64 @@ $(document).ready(function () {
 
 var Playlistify = {
     oauthURI: "https://accounts.spotify.com/authorize?client_id=7f034be8c85340a9a3179b195bfa343f&redirect_uri=http://webshare.mah.se/af8654/redirectIndex.html&scope=user-top-read&response_type=token",
-    
-    access_token:"",
+
+    access_token: "",
 
     topArtistsId: [],
+    checkedArray: [],
 
-    init: function(){
-        $("#redirectBtn").click(function() {
+    init: function () {
+        $("#redirectBtn").click(function () {
             Playlistify.oauth();
         });
+
+        $(".createPlaylist").click(function () {
+            Playlistify.getChecked();
+        });
+
+
         var s = window.location.hash.split('=');
         var key = s[1].split('&');
         Playlistify.access_token = key[0];
         Playlistify.getTopArtists();
     },
 
-    oauth: function(){
-            window.location.replace(Playlistify.oauthURI);
-            //console.log(Playlistify.getUrlVars());
+    oauth: function () {
+        window.location.replace(Playlistify.oauthURI);
+        //console.log(Playlistify.getUrlVars());
     },
 
-    getTopArtists: function(){
+    getTopArtists: function () {
+        var indexNr = 0;
         $.ajax({
-            url:"https://api.spotify.com/v1/me/top/artists",
+            url: "https://api.spotify.com/v1/me/top/artists",
             type: "GET",
-            headers: {"Authorization": "Bearer " + Playlistify.access_token},
-            success: function(result){
+            headers: { "Authorization": "Bearer " + Playlistify.access_token },
+            success: function (result) {
                 // document.getElementById("createText").innerHTML = result.items[0].name;
                 for (let i = 0; i < result.items.length; i++) {
-                    $('#createText').append('<li class = "listItem">' + result.items[i].name + "</li><br>");
-                    Playlistify.topArtistsId.push(result.items[i].id)
+                    $('#createText').append('<p class = "listItem">' + result.items[i].name +
+                        "</p><br>");
+                    Playlistify.topArtistsId.push(result.items[i].id);
                 }
+
+                $(".listItem").each(function () {
+                    $(this).append('<input type="checkbox" value="' + indexNr + '" class = "ml-3">');
+                    indexNr++;
+                })
                 console.log(result);
                 console.log(Playlistify.topArtistsId);
+
             }
         })
+    },
+
+    getChecked: function () {
+        $("input:checked").each(function () {
+            Playlistify.checkedArray.push($(this).val());
+        });
+        
+        console.log(Playlistify.checkedArray);
     }
 }
+
